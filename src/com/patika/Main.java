f57015fa91848f8f1b77a032e1de21521848fd14
HaleGurpinar <106753721+HaleGurpinar@users.noreply.github.com>
2023-04-20 14:51:00 +0300

    Create generic class

--- src/com/patika/Main.java
+++ src/com/patika/Main.java
@@ -3,6 +3,27 @@ package com.patika;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        MyList<Integer> myList= new MyList<>();
+        System.out.println("List Case : " + (myList.isEmpty() ? "Empty" : "Full"));
+        myList.add(20);
+        myList.add(30);
+        myList.add(40);
+        System.out.println("Number of Elements in Array : " + myList.size());
+        System.out.println("Capacity of Array : " + myList.getCapacity());
+        System.out.println("Number of Elements in Array : " + myList.size());
+        System.out.println("Capacity of Array : " + myList.getCapacity());
+        myList.add(50);
+        myList.add(60);
+        myList.add(70);
+        myList.add(80);
+        myList.add(90);
+       /* myList.add(100);
+        myList.add(110);*/
+        System.out.println(myList);
+        System.out.println("Number of Elements in Array : " + myList.size());
+        System.out.println("Capacity of Array : " + myList.getCapacity());
+
+
     }
 }
--- src/com/patika/MyList.java
+++ src/com/patika/MyList.java
@@ -4,25 +4,62 @@ import java.lang.reflect.Array;
 
  class MyList<T>{
 
-    private int capacity;
+    private int capacity=10;
     private int size;
 
-    private Object[] array;
+     T[] list= (T[]) new Object[capacity];
 
     public MyList() {
-        array=new Object[10];
-
+       T[] list=(T[]) new Object[10];
     }
 
     public MyList(int capacity) {
-
+        this.capacity=capacity;
+        T[] list= (T[]) new Object[capacity];
     }
 
-    public int size(int size){
-        return array.length;
+    public int size(){
+        int c=0;
+        for (T t : list) {
+            if (t != null) {
+                c++;
+            }
+        }
+        return c;
     }
 
     public int getCapacity() {
         return capacity;
     }
-}
+    public void add(T data) {
+        if (this.size==this.getCapacity()){
+            this.capacity=2*capacity;
+        T[] newList=(T[]) new Object[capacity];
+        for (int i=0;i< list.length;i++){
+            newList[i]=list[i];
+        }
+        newList[list.length]=data;
+        this.list=newList;
+        } else{
+            list[this.size()]=data;
+        }
+    }
+
+     public boolean isEmpty(){
+         int count =0;
+         for (int i = 0; i < list.length ; i++) {
+             if(list[i]==null){
+                 count++;
+             }
+
+         }if(count==list.length){
+             return true;
+         }
+
+         else{
+             return false;
+         }
+     }
+
+    }
+
