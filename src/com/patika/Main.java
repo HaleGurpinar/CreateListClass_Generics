bcf292b4a9ca3f605af63ff4268491668c8655a9
HaleGurpinar <106753721+HaleGurpinar@users.noreply.github.com>
2023-04-27 20:21:44 +0300

    complete methods

--- src/com/patika/Main.java
+++ src/com/patika/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
         MyList<Integer> myList= new MyList<>();
         System.out.println("List Case : " + (myList.isEmpty() ? "Empty" : "Full"));
+        System.out.println("Number of Elements in Array : " + myList.size());
         myList.add(10);
         myList.add(20);
         myList.add(30);
@@ -16,31 +17,38 @@ public class Main {
         myList.add(80);
         myList.add(90);
         myList.add(100);
-        //myList.add(110);
+        myList.add(110);
+        myList.add(120);
+
         System.out.println("Number of Elements in Array : " + myList.size());
-        System.out.println("Capacity of Array : " + myList.getCapacity());
-        System.out.println(myList.toString());
         System.out.println("List Case : " + (myList.isEmpty() ? "Empty" : "Full"));
-        System.out.println("Number of Elements in Array : " + myList.size());
-        System.out.println("Capacity of Array : " + myList.getCapacity());
-        System.out.println(myList.get(3));
-        myList.remove(3);
-        System.out.println(myList.get(4));
-        myList.set(3,55);
-        System.out.println(myList.get(3));
-        System.out.println(myList.get(4));
         System.out.println(myList.toString());
-        System.out.println(myList.indexOf(60));
-        System.out.println(myList.lastIndexOf(30));
-        Object[] array= myList.toArray();
-        System.out.println(array[0]);
-        System.out.println(myList.sublist(0,2));
-        System.out.println(myList.contains(90));
 
+        // Return index of entered value
+        System.out.println("Index : " + myList.indexOf(40));
 
+        // Return -1 if method can not find value
+        System.out.println("Index :" + myList.indexOf(140));
 
+        // Return last index of list
+        System.out.println("Index : " + myList.lastIndexOf(20));
 
+        // Listeyi Object[] dizisi olarak geri verir.
+        Object[] dizi = myList.toArray();
+        System.out.println("First element of Object array :" + dizi[0]);
 
+        // Create a sublist with list type
+        MyList<Integer> subList = myList.subList(0, 3);
+        System.out.println(subList.toString());
+
+        // Number in list or not
+        System.out.println("20 value in my list: " + myList.contains(20));
+        System.out.println("105 value in my list: " + myList.contains(105));
+
+        // Clear all list and assign null
+        myList.clear();
+        System.out.println(myList.toString());
+    }
 
     }
-}
+
--- src/com/patika/MyList.java
+++ src/com/patika/MyList.java
@@ -1,177 +1,145 @@
 package com.patika;
-
+import java.util.ArrayList;
 import java.util.Arrays;
 
-class MyList<T>{
-
-    private int capacity;
-    private int size=0;
-
-     private T[] list;
+public class MyList<T> {
+    T[] array;
 
     public MyList() {
-       setCapacity(10);
-       this.list=(T[]) new Object[getCapacity()];
+        this.array = (T[]) new Object[10];
     }
 
     public MyList(int capacity) {
-        this.size=0;
-        this.capacity=capacity;
-        this.list= (T[]) new Object[capacity];
+
+        this.array = (T[]) new Object[capacity];
     }
 
-    public int size(){
-        int size=0;
-        for (T t : list) {
-            if (t != null) {
-                size++;
+    public int size() {
+        int count = 0;
+        for (T element : array) {
+            if (element != null) {
+                count++;
             }
         }
-        return size;
+        return count;
     }
 
-    public  int indexOf(T data){
-        int curIndex=0;
-
-        while (curIndex< size){
-            if (list[curIndex] == data)
-                return curIndex;
-            curIndex++;
-        }
-        return -1;
+    public int getCapacity() {
+        return array.length;
     }
 
-    public  int lastIndexOf(T data){
-        int curIndex=size-1;
-
-        while (curIndex>=0){
-            if (list[curIndex] == data)
-                return curIndex;
-            curIndex--;
-        }
-        return -1;
-    }
-
-    public T[] toArray(){
-
-        T[] items= (T[]) new Object[size];
-
-        for (int i=0;i<size;i++){
-            items[i]=list[i];
-        }
-        return items;
-    }
-
-    public void clear(){
-        for (int i=0;i<=list.length;i++){
-            list[i]=null;
+    public void add(T data) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == null) {
+                array[i] = data;
+                break;
+            } else if (array[i] != null && i == array.length - 1) {
+                array = Arrays.copyOf(array, array.length * 2);
+                array[array.length / 2] = data;
+                break;
+            }
         }
     }
 
-    public MyList <T> sublist(int start,int finish){
-        int newSize=finish-start+1;
-        MyList <T> newList= new MyList<>(newSize);
+    public T get(int index) {
+        try {
+            if (array[index] != null) {
+                return array[index];
+            } else return null;
+        } catch (Exception ignored) {
+            return null;
+        }
 
-        for (int i=start;i<=finish;i++){
-            newList.add(list[i]);
+
+    }
+
+    public T remove(int index) {
+        T value;
+        try {
+            value=get(index);
+            if (array[index] != null) {
+                if (size() - index >= 0) System.arraycopy(array, index + 1, array, index, size() - index);
+            }
+        } catch (Exception ignored) {
+            return null;
+        }
+        return value;
+    }
+
+    public void set(int index, T data) {
+        try {
+            if (array[index] != null) {
+                array[index] = data;
+            }
+        } catch (Exception ignored) {
+            System.out.println("Hatalı işlem");
+        }
+    }
+
+    public String toString() {
+        if (size() > 0) {
+            StringBuilder str = new StringBuilder("[");
+            for (int i = 0; i < size(); i++) {
+                if (i == (size() - 1)) {
+                    str.append(array[i]).append("]");
+                } else str.append(array[i]).append(",");
+
+            }
+            return str.toString();
+        }
+        return "[]";
+    }
+
+    public int indexOf(T data) {
+        int index = -1;
+        for (int i = 0; i < size(); i++) {
+            if (array[i] == data) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    public int lastIndexOf(T data) {
+        int index = -1;
+        for (int i = size() - 1; i >= 0; i--) {
+            if (array[i] == data) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public T[] toArray() {
+        return Arrays.copyOfRange(array, 0, size());
+    }
+
+    public void clear() {
+        this.array = (T[]) new Object[10];
+    }
+
+    public MyList<T> subList(int start, int finish) {
+        MyList<T> newList = new MyList<>((finish - start + 1));
+        for (int i = start; i <= finish; i++) {
+            newList.add(array[i]);
         }
         return newList;
     }
 
-    public boolean contains(T data){
-        return indexOf(data)!= -1;
-    }
-
-    public void add(T data) {
-
-         if (capacity >= size) {
-             this.list[size] = data;
-             setSize(getSize() + 1);
-         } else {
-           T[]  tempList = (T[]) new Object[(this.capacity)];
-             setCapacity(getCapacity() * 2);
-
-             int index = 0;
-             for (T val : list) {
-                 tempList[index] = val;
-                 index++;
-             }
-
-             list = (T[]) new Object[(getCapacity())];
-
-             index = 0;
-             for (T val : tempList) {
-                 list[index] = val;
-                 index++;
-             }
-
-             this.list[size()] = data;
-             setSize(getSize() + 1);
-         }
-       /* if (this.size>=this.capacity){
-            int newCapacity= capacity*2;
-            T[] newList=(T[]) new Object[(newCapacity)];
-            System.arraycopy(list, 0, newList, 0, list.length);
-            list=newList;
-        }else{
-            list[size++]= data;
-        }*/
-
-    }
-
-    public T get(int index){
-        if (index<=getSize()){
-            return list[index];
-        } else {
-            return null;
-        }
-    }
-
-    public void remove(int index){
-        if (index < 0 || index > this.getCapacity()){
-            for (int i=index+1;i<this.size();i++){
-                this.list[i-1]= this.list[i];
-                list[index]=null;
+    public boolean contains(T data) {
+        boolean isIt = false;
+        for (int i = 0; i < size(); i++) {
+            if (array[i] == data) {
+                isIt = true;
+                break;
             }
-        }else{
-            return;
         }
-       // setSize(getSize() - 1);
+        return isIt;
     }
-
-    public T set(int index, T data){
-        if (index < 0 || index > size){
-            return null;
-        }else {
-            return list[index] = data;
-        }
-
-        }
-
-
-     @Override
-     public String toString() {
-         return Arrays.toString(list);
-     }
-
-     public boolean isEmpty(){
-         return size==0;
-         }
-
-     public int getSize() {
-         return size;
-     }
-
-     public void setSize(int size) {
-         this.size = size;
-     }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
- }
-
+}
